84f0994b25d78c05d7d8f13e8c6abc0bc239e328
Lukas <lucasfasilva27@gmail.com>
2022-01-29 10:36:36 -0300

    Product insert, update and delete

--- backend/src/main/java/com/dvsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/dvsuperior/dscatalog/services/ProductService.java
@@ -12,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dvsuperior.dscatalog.dto.CategoryDTO;
 import com.dvsuperior.dscatalog.dto.ProductDTO;
+import com.dvsuperior.dscatalog.entities.Category;
 import com.dvsuperior.dscatalog.entities.Product;
+import com.dvsuperior.dscatalog.repositories.CategoryRepository;
 import com.dvsuperior.dscatalog.repositories.ProductRepository;
 import com.dvsuperior.dscatalog.services.exceptions.DataBaseException;
 import com.dvsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -25,6 +28,9 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -43,16 +49,18 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-//		entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 	}
 	
+	
+
 	@Transactional
 	public ProductDTO update(Long id,ProductDTO dto) {
 		try {
 		Product entity = repository.getOne(id);
-//		entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 		}
@@ -72,6 +80,21 @@ public class ProductService {
 			throw new DataBaseException("Integrity violation");
 		}
 		
+	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
+		}
 	}
 
 	
